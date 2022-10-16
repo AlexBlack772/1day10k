f471766a08d376b5f64d205ca8aad9ffee0db460
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-16 09:09:00 +0800

    1015

---
+++ 1015/cal.go
@@ -0,0 +1,405 @@
+package 1015
+//2つの合計値がyになる関数
+func sum(x, y int) (int, int) {
+	 return y - x, x
+}
+//素数の判定の関数
+func isPrime(x int) bool {
+	 for i := 2; i < x; i++ {
+		  if x%i == 0 {
+				return false
+		  }
+	 }
+	 return true
+}
+//素因数分解の関数
+func primeFactorization(x int) []int {
+	 var ret []int
+	 for i := 2; i <= x; i++ {
+		  for x%i == 0 {
+				ret = append(ret, i)
+				x /= i
+		  }
+	 }
+	 return ret
+}
+//素数の列挙の関数
+func primeList(x int) []int {
+	 var ret []int
+	 for i := 2; i <= x; i++ {
+		  if isPrime(i) {
+				ret = append(ret, i)
+		  }
+	 }
+	 return ret
+}
+//階乗の関数
+func factorial(x int) int {
+	 ret := 1
+	 for i := 2; i <= x; i++ {
+		  ret *= i
+	 }
+	 return ret
+}
+//最大公約数の関数
+func gcd(x, y int) int {
+	 for y != 0 {
+		  x, y = y, x%y
+	 }
+	 return x
+}
+//最小公倍数の関数
+func lcm(x, y int) int {
+	 return x * y / gcd(x, y)
+}
+//約数の列挙の関数
+func divisor(x int) []int {
+	 var ret []int
+	 for i := 1; i*i <= x; i++ {
+		  if x%i == 0 {
+				ret = append(ret, i)
+				if i*i != x {
+					 ret = append(ret, x/i)
+				}
+		  }
+	 }
+	 return ret
+}
+//約数の個数の関数
+func divisorCount(x int) int {
+	 ret := 0
+	 for i := 1; i*i <= x; i++ {
+		  if x%i == 0 {
+				ret++
+				if i*i != x {
+					 ret++
+				}
+		  }
+	 }
+	 return ret
+}
+//約数の和の関数
+func divisorSum(x int) int {
+	 ret := 0
+	 for i := 1; i*i <= x; i++ {
+		  if x%i == 0 {
+				ret += i
+				if i*i != x {
+					 ret += x / i
+				}
+		  }
+	 }
+	 return ret
+}
+//約数の列挙の関数
+func divisorList(x int) []int {
+	 var ret []int
+	 for i := 1; i*i <= x; i++ {
+		  if x%i == 0 {
+				ret = append(ret, i)
+				if i*i != x {
+					 ret = append(ret, x/i)
+				}
+		  }
+	 }
+	 return ret
+}
+//型エイリアス
+type Int int
+//オブジェクト型
+type Int struct {
+	 x int
+}
+//コンストラクタ
+func NewInt(x int) Int {
+	 return Int{x}
+}
+//配列の最小値の関数
+func minArray(x []int) int {
+	 ret := x[0]
+	 for _, v := range x {
+		  ret = min(ret, v)
+	 }
+	 return ret
+}
+//配列の最大値の関数
+func maxArray(x []int) int {
+	 ret := x[0]
+	 for _, v := range x {
+		  ret = max(ret, v)
+	 }
+	 return ret
+}
+//配列の和の関数
+func sumArray(x []int) int {
+	 ret := 0
+	 for _, v := range x {
+		  ret += v
+	 }
+	 return ret
+}
+//配列の積の関数
+func productArray(x []int) int {
+	 ret := 1
+	 for _, v := range x {
+		  ret *= v
+	 }
+	 return ret
+}
+//配列の２番目に大きい値の関数
+func secondMaxArray(x []int) int {
+	 ret := x[0]
+	 ret2 := x[0]
+	 for _, v := range x {
+		  if ret < v {
+				ret2 = ret
+				ret = v
+		  } else if ret2 < v {
+				ret2 = v
+		  }
+	 }
+	 return ret2
+}
+//配列の２番目に小さい値の関数
+func secondMinArray(x []int) int {
+	 ret := x[0]
+	 ret2 := x[0]
+	 for _, v := range x {
+		  if ret > v {
+				ret2 = ret
+				ret = v
+		  } else if ret2 > v {
+				ret2 = v
+		  }
+	 }
+	 return ret2
+}
+//配列のソートの関数
+func sortArray(x []int) []int {
+	 sort.Ints(x)
+	 return x
+}
+//文字列を改行する関数
+func splitString(x string) []string {
+	 return strings.Split(x, "
+
+")
+}
+//文字列をスペースで分割する関数
+func splitStringSpace(x string) []string {
+	 return strings.Split(x, " ")
+}
+//文字列をスライスにする関数
+func splitStringSlice(x string) []string {
+	 return strings.Split(x, "")
+}
+//数値を繰り返したした関数
+func repeatString(x string, y int) string {
+	 return strings.Repeat(x, y)
+}
+//文字列を結合する関数
+func joinString(x []string, y string) string {
+	 return strings.Join(x, y)
+}
+//整数の１の位
+func ones(x int) int {
+	 return x % 10
+}
+//整数の１０の位
+func tens(x int) int {
+	 return x / 10 % 10
+}
+//整数の１００の位
+func hundreds(x int) int {
+	 return x / 100 % 10
+}	
+//配列をコピーして、新しい配列を返す関数
+func copyArray(x []int) []int {
+	 y := make([]int, len(x))
+	 copy(y, x)
+	 return y
+}
+//配列の中に要素を追加して、新しい配列を作成する関数
+func appendArray(x []int, y int) []int {
+	 z := make([]int, len(x)+1)
+	 copy(z, x)
+	 z[len(x)] = y
+	 return z
+}
+//２つの配列を結合して、新しい配列を作成する関数
+func concatArray(x []int, y []int) []int {
+	 z := make([]int, len(x)+len(y))
+	 copy(z, x)
+	 copy(z[len(x):], y)
+	 return z
+}
+//配列の中に要素があるかどうかを判定する関数
+func containsArray(x []int, y int) bool {
+	 for _, v := range x {
+		  if v == y {
+				return true
+		  }
+	 }
+	 return false
+}
+//配列の中に要素があるかどうかを判定する関数
+func containsArrayString(x []string, y string) bool {
+	 for _, v := range x {
+		  if v == y {
+				return true
+		  }
+	 }
+	 return false
+}
+//配列の中に要素があるかどうかを判定する関数
+func containsArrayRune(x []rune, y rune) bool {
+	 for _, v := range x {
+		  if v == y {
+				return true
+		  }
+	 }
+	 return false
+}
+//配列オブジェクトのマージ
+func mergeArray(x []int, y []int) []int {
+	 return append(x, y...)
+}
+//配列の中に要素があるかどうかを判定する関数
+func containsArrayInt(x []Int, y Int) bool {
+	 for _, v := range x {
+		  if v == y {
+				return true
+		  }
+	 }
+	 return false
+}
+//アラートを表示する関数
+func alert(x string) {
+	 fmt.Println(x)
+}
+//動作確認アラートを表示する関数
+func alertTest(x string) {
+	 fmt.Println(x)
+}
+//配列の中に要素があるかどうかを判定する関数
+func containsArrayString(x []string, y string) bool {
+	 for _, v := range x {
+		  if v == y {
+				return true
+		  }
+	 }
+	 return false
+}
+//配列から選んで並び替えた中身を返す関数
+func selectArray(x []int, y []int) []int {
+	 z := []int{}
+	 for _, v := range y {
+		  z = append(z, x[v])
+	 }
+	 return z
+}
+//全ての順列を返す関数
+func permutationArray(x []int) [][]int {
+	 return permutation(x)
+}
+//配列から同じ数値を２つ選ぶ場合の数を返す関数
+func combinationArray(x []int) [][]int {
+	 return combination(x)
+}
+//サイコロの出目の総和を返す関数
+func diceSum(x int) int {
+	 return x * 7
+}
+//サイコロの出目が等しくなる確率を返す関数
+func diceEqual(x int) float64 {
+	 return 1 / float64(x)
+}
+//サイコロの出目が等しくならない確率を返す関数
+func diceNotEqual(x int) float64 {
+	 return 1 - diceEqual(x)
+}
+//サイコロの出目が等しくなる確率を返す関数
+func diceEqual(x int) float64 {
+	 return 1 / float64(x)
+}
+//サイコロの出目が等しくならない確率を返す関数
+func diceNotEqual(x int) float64 {
+	 return 1 - diceEqual(x)
+}
+//2で割り切れるかどうかを判定する関数
+func isEven(x int) bool {
+	 return x%2 == 0
+}
+//2で割り切れないかどうかを判定する関数
+func isOdd(x int) bool {
+	 return x%2 == 1
+}
+//配列の中に要素があるかどうかを判定する関数
+func containsArrayInt(x []Int, y Int) bool {
+	 for _, v := range x {
+		  if v == y {
+				return true
+		  }
+	 }
+	 return false
+}
+//配列の中に要素があるかどうかを判定する関数
+func containsArrayString(x []string, y string) bool {
+	 for _, v := range x {
+		  if v == y {
+				return true
+		  }
+	 }
+	 return false
+}
+//コンマの数を返す関数
+func countComma(x string) int {
+	 return strings.Count(x, ",")
+}
+//2進数のビット反転を返す関数
+func bitReverse(x int) int {
+	 return ^x
+}
+//２進数の２の補数を返す関数
+func bitComplement(x int) int {
+	 return ^x + 1
+}
+//２進数のビット反転を返す関数
+func bitReverse(x int) int {
+	 return ^x
+}
+//xかつyの倍数を返す関数
+func multiple(x int, y int) int {
+	 return x * y
+}
+//線形探索を行う関数
+func linearSearch(x []int, y int) int {
+	 for i, v := range x {
+		  if v == y {
+				return i
+		  }
+	 }
+	 return -1
+}
+//2進数を10進数に変換する関数
+func binaryToDecimal(x string) int {
+	 i, _ := strconv.ParseInt(x, 2, 64)
+	 return int(i)
+}
+//10進数を2進数に変換する関数
+func decimalToBinary(x int) string {
+	 return strconv.FormatInt(int64(x), 2)
+}
+//最大値と最小値を返す関数
+//５より大きく２０より小さい数を返す関数
+func between(x int, y int, z int) int {
+	 if x < y && y < z {
+		  return y
+	 }
+	 return 0
+}
+// １からnまたはnから１までの和を返す関数
+func sum(x int) int {
+	 return (x + 1) * x / 2
+}
+
--- 1015/main.go
+++ 1015/main.go
@@ -31,4 +31,497 @@ func join(s []string, sep string) string {
 	 }
 	 return ret
 }
-//
+//2つの配列の中身を比較する関数
+func equal(x, y []string) bool {
+	 if len(x) != len(y) {
+		  return false
+	 }
+	 for i := range x {
+		  if x[i] != y[i] {
+				return false
+		  }
+	 }
+	 return true
+}
+//文字列の抽出の関数
+func slice(s string, low, high int) string {
+	 if low < 0 {
+		  low = 0
+	 }
+	 if high > len(s) {
+		  high = len(s)
+	 }
+	 return s[low:high]
+}
+//文字列の置換の関数
+func replace(s, old, new string) string {
+	 var ret string
+	 for i := 0; i < len(s); {
+		  if strings.HasPrefix(s[i:], old) {
+				ret += new
+				i += len(old)
+		  } else {
+				ret += string(s[i])
+				i++
+		  }
+	 }
+	 return ret
+}
+//引数を+1する関数
+func inc(x int) int {
+	 return x + 1
+}
+//引数を-1する関数
+func dec(x int) int {
+	 return x - 1
+}
+//引数を2倍する関数
+func double(x int) int {
+	 return x * 2
+}
+//引数の文字数を判定する関数
+func count(s string) int {
+	 return len(s)
+}
+//空の配列を定義する
+var empty []string
+//配列の最初の要素を削除する
+func removeFirst(s []string) []string {
+	 return s[1:]
+}
+//配列の３番目の要素を削除する
+func removeThird(s []string) []string {
+	 return append(s[:2], s[3:]...)
+}
+//配列の最後の要素を削除する
+func removeLast(s []string) []string {
+	 return s[:len(s)-1]
+}
+//配列の最初の要素を追加する
+func addFirst(s []string, e string) []string {
+	 return append([]string{e}, s...)
+}
+//配列の３番目の要素を追加する
+func addThird(s []string, e string) []string {
+	 return append(s[:2], append([]string{e}, s[2:]...)...)
+}
+//配列の最後の要素を追加する
+func addLast(s []string, e string) []string {
+	 return append(s, e)
+}
+//配列の逆順にする
+func reverse(s []string) []string {
+	 var ret []string
+	 for i := len(s) - 1; i >= 0; i-- {
+		  ret = append(ret, s[i])
+	 }
+	 return ret
+}
+//配列の要素をシャッフルする
+func shuffle(s []string) []string {
+	 var ret []string
+	 for len(s) > 0 {
+		  i := rand.Intn(len(s))
+		  ret = append(ret, s[i])
+		  s = append(s[:i], s[i+1:]...)
+	 }
+	 return ret
+}
+//配列の要素をソートする
+func sort(s []string) []string {
+	 sort.Strings(s)
+	 return s
+}
+//配列ないの要素を重複を除いてソートする
+func uniq(s []string) []string {
+	 sort.Strings(s)
+	 var ret []string
+	 for i := 0; i < len(s); i++ {
+		  if i == 0 || s[i-1] != s[i] {
+				ret = append(ret, s[i])
+		  }
+	 }
+	 return ret
+}
+//配列の要素を重複を除いてソートする
+func uniq2(s []string) []string {
+	 sort.Strings(s)
+	 var ret []string
+	 for i := 0; i < len(s); i++ {
+		  if i == 0 || s[i-1] != s[i] {
+				ret = append(ret, s[i])
+		  }
+	 }
+	 return ret
+}
+//配列オブジェクト定義する
+type array []string
+//配列の要素を重複を除いてソートする
+func (s array) uniq() array {
+	 sort.Strings(s)
+	 var ret []string
+	 for i := 0; i < len(s); i++ {
+		  if i == 0 || s[i-1] != s[i] {
+				ret = append(ret, s[i])
+		  }
+	 }
+	 return ret
+}
+//非破壊的な配列の要素を重複を除いてソートする
+func (s array) uniq2() array {
+	 sort.Strings(s)
+	 var ret []string
+	 for i := 0; i < len(s); i++ {
+		  if i == 0 || s[i-1] != s[i] {
+				ret = append(ret, s[i])
+		  }
+	 }
+	 return ret
+}
+//非破壊的繰り返しpush
+func (s array) push(e string) array {
+	 return append(s, e)
+}
+//非破壊的繰り返しpop
+func (s array) pop() (array, string) {
+	 return s[:len(s)-1], s[len(s)-1]
+}
+//非破壊的繰り返しshift
+func (s array) shift() (array, string) {
+	 return s[1:], s[0]
+}
+//非破壊的繰り返しunshift
+func (s array) unshift(e string) array {
+	 return append([]string{e}, s...)
+}
+//非破壊的繰り返しreverse
+func (s array) reverse() array {
+	 var ret []string
+	 for i := len(s) - 1; i >= 0; i-- {
+		  ret = append(ret, s[i])
+	 }
+	 return ret
+}
+//非破壊的繰り返しshuffle
+func (s array) shuffle() array {
+	 var ret []string
+	 for len(s) > 0 {
+		  i := rand.Intn(len(s))
+		  ret = append(ret, s[i])
+		  s = append(s[:i], s[i+1:]...)
+	 }
+	 return ret
+}
+//非破壊的繰り返しsort
+func (s array) sort() array {
+	 sort.Strings(s)
+	 return s
+}
+//非破壊的繰り返しuniq
+func (s array) uniq3() array {
+	 sort.Strings(s)
+	 var ret []string
+	 for i := 0; i < len(s); i++ {
+		  if i == 0 || s[i-1] != s[i] {
+				ret = append(ret, s[i])
+		  }
+	 }
+	 return ret
+}
+//非破壊的繰り返しuniq
+func (s array) uniq4() array {
+	 sort.Strings(s)
+	 var ret []string
+	 for i := 0; i < len(s); i++ {
+		  if i == 0 || s[i-1] != s[i] {
+				ret = append(ret, s[i])
+		  }
+	 }
+	 return ret
+}
+//非破壊的fill
+func (s array) fill(e string) array {
+	 for i := 0; i < len(s); i++ {
+		  s[i] = e
+	 }
+	 return s
+}
+//var排除する
+func (s array) var() array {
+	 var ret []string
+	 for i := 0; i < len(s); i++ {
+		  if s[i] != "var" {
+				ret = append(ret, s[i])
+		  }
+	 }
+	 return ret
+}
+//配列の最小値を返す
+func (s array) min() string {
+	 sort.Strings(s)
+	 return s[0]
+}
+//配列の最大値を返す
+func (s array) max() string {
+	 sort.Strings(s)
+	 return s[len(s)-1]
+}
+//配列の要素を重複を除いてソートする
+func (s array) uniq5() array {
+	 sort.Strings(s)
+	 var ret []string
+	 for i := 0; i < len(s); i++ {
+		  if i == 0 || s[i-1] != s[i] {
+				ret = append(ret, s[i])
+		  }
+	 }
+	 return ret
+}
+//配列の２番目に小さい値を返す
+func (s array) second() string {
+	 sort.Strings(s)
+	 return s[1]
+}
+//配列の３番目に小さい値を返す
+func (s array) third() string {
+	 sort.Strings(s)
+	 return s[2]
+}
+//文字列のn文字目の文字を返す
+func (s array) charAt(n int) string {
+	 return string(s[n])
+}
+//文字列を改行する関数
+func (s array) nl() string {
+	 return string(s) + "
+
+"
+}
+//文字列を改行する関数
+func (s array) nl2() string {
+	 return string(s) + "
+
+"
+}
+//数値を繰り返したした値
+func (s array) repeat(n int) string {
+	 var ret string
+	 for i := 0; i < n; i++ {
+		  ret += string(s)
+	 }
+	 return ret
+}
+//整数の桁数
+func (s array) digit() int {
+	 return len(s)
+}
+//整数の階乗
+func (s array) factorial() int {
+	 n, _ := strconv.Atoi(string(s))
+	 if n == 0 {
+		  return 1
+	 }
+	 return n * array(strconv.Itoa(n-1)).factorial()
+}
+//整数の階乗
+func (s array) factorial2() int {
+	 n, _ := strconv.Atoi(string(s))
+	 if n == 0 {
+		  return 1
+	 }
+	 return n * array(strconv.Itoa(n-1)).factorial()
+}
+//数値列挙1-100する関数
+func (s array) range() array {
+	 var ret []string
+	 for i := 0; i < 100; i++ {
+		  ret = append(ret, strconv.Itoa(i))
+	 }
+	 return ret
+}
+//数値列挙1-100する関数
+func (s array) range2() array {
+	 var ret []string
+	 for i := 0; i < 100; i++ {
+		  ret = append(ret, strconv.Itoa(i))
+	 }
+	 return ret
+}
+//文字列列挙a~zする関数
+func (s array) range3() array {
+	 var ret []string
+	 for i := 97; i < 123; i++ {
+		  ret = append(ret, string(i))
+	 }
+	 return ret
+}
+//fizzbuzzする関数
+func (s array) fizzbuzz() array {
+	 var ret []string
+	 for i := 1; i < 101; i++ {
+		  if i%15 == 0 {
+				ret = append(ret, "FizzBuzz")
+		  } else if i%3 == 0 {
+				ret = append(ret, "Fizz")
+		  } else if i%5 == 0 {
+				ret = append(ret, "Buzz")
+		  } else {
+				ret = append(ret, strconv.Itoa(i))
+		  }
+	 }
+	 return ret
+}
+//配列の要素を重複を除いてソートする
+func (s array) uniq6() array {
+//二次元配列の和を求める
+func (s array) sum() int {
+	 var ret int
+	 for i := 0; i < len(s); i++ {
+		  ret += s[i]
+	 }
+	 return ret
+}
+//二次元配列の積を求める
+func (s array) product() int {
+	 var ret int
+	 for i := 0; i < len(s); i++ {
+		  ret *= s[i]
+	 }
+	 return ret
+}
+//二次元配列の最小値を求める
+func (s array) min2() int {
+	 sort.Ints(s)
+	 return s[0]
+}
+//二次元配列の最大値を求める
+func (s array) max2() int {
+	 sort.Ints(s)
+	 return s[len(s)-1]
+}
+//二次元配列の要素を重複を除いてソートする
+func (s array) uniq7() array {
+	 sort.Ints(s)
+	 var ret []int
+	 for i := 0; i < len(s); i++ {
+		  if i == 0 || s[i-1] != s[i] {
+				ret = append(ret, s[i])
+		  }
+	 }
+	 return ret
+}
+//二次元配列の２番目に小さい値を求める
+func (s array) second2() int {
+	 sort.Ints(s)
+	 return s[1]
+}
+//二次元配列の３番目に小さい値を求める
+func (s array) third2() int {
+	 sort.Ints(s)
+	 return s[2]
+}
+//二次元配列の要素を重複を除いてソートする
+func (s array) uniq8() array {
+	 sort.Ints(s)
+	 var ret []int
+	 for i := 0; i < len(s); i++ {
+		  if i == 0 || s[i-1] != s[i] {
+				ret = append(ret, s[i])
+		  }
+	 }
+	 return ret
+}
+//二次元配列の２番目に小さい値を求める
+func (s array) second3() int {
+	 sort.Ints(s)
+	 return s[1]
+}
+//二次元配列の３番目に小さい値を求める
+func (s array) third3() int {
+	 sort.Ints(s)
+	 return s[2]
+}
+//二次元配列の要素を重複を除いてソートする
+func (s array) uniq9() array {
+	 sort.Ints(s)
+	 var ret []int
+	 for i := 0; i < len(s); i++ {
+		  if i == 0 || s[i-1] != s[i] {
+				ret = append(ret, s[i])
+		  }
+	 }
+	 return ret
+}
+//二次元配列の２番目に小さい値を求める
+func (s array) second4() int {
+	 sort.Ints(s)
+	 return s[1]
+}
+//二次元配列の３番目に小さい値を求める
+func (s array) third4() int {
+	 sort.Ints(s)
+	 return s[2]
+}
+//二次元配列の要素を重複を除いてソートする
+func (s array) uniq10() array {
+	 sort.Ints(s)
+	 var ret []int
+	 for i := 0; i < len(s); i++ {
+		  if i == 0 || s[i-1] != s[i] {
+				ret = append
+
+ret, s[i])
+//二次元配列の２番目に小さい値を求める
+func (s array) second5() int {
+	 sort.Ints(s)
+	 return s[1]
+}
+//二次元配列の３番目に小さい値を求める
+func (s array) third5() int {
+	 sort.Ints(s)
+	 return s[2]
+}
+//二次元配列の要素を重複を除いてソートする
+func (s array) uniq11() array {
+	 sort.Ints(s)
+	 var ret []int
+	 for i := 0; i < len(s); i++ {
+		  if i == 0 || s[i-1] != s[i] {
+				ret = append(ret, s[i])
+		  }
+	 }
+	 return ret
+}
+//二次元配列の２番目に小さい値を求める
+func (s array) second6() int {
+	 sort.Ints(s)
+	 return s[1]
+}
+//二次元配列の３番目に小さい値を求める
+func (s array) third6() int {
+	 sort.Ints(s)
+	 return s[2]
+}
+//二次元配列の要素を重複を除いてソートする
+func (s array) uniq12() array {
+	 sort.Ints(s)
+	 var ret []int
+	 for i := 0; i < len(s); i++ {
+		  if i == 0 || s[i-1] != s[i] {
+				ret = append(ret, s[i])
+		  }
+	 }
+	 return ret
+}
+//二次元配列の２番目に小さい値を求める
+func (s array) second7() int {
+	 sort.Ints(s)
+	 return s[1]
+}
+//二次元配列の３番目に小さい値を求める
+func (s array) third7() int {
+	 sort.Ints(s)
+	 return s[2]
+}
+//２つの合計がyになる計算式の関数
+func 
