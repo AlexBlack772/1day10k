b1005a1ba1172a0f6c177e903e56042fe4502394
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-25 09:01:22 +0800

    10k

---
+++ 202210/1021-1031/1024/open.sol
@@ -0,0 +1,104 @@
+//erc1155puasableとは、ERC1155のトークンを所有しているアドレスが、トークンを送信する前に、そのトークンを送信することを許可することができるようにするための機能です。
+bool public isPaused;
+baseURI = "https://api.cryptokitties.co/kitties/";
+balanceof[owner] = totalSupply;
+//transfersingleは、ERC1155トークンを所有しているアドレスから別のアドレスに送信するための関数です。
+transfersingle(msg.sender, to, id, value, data);
+//transferbatchは、ERC1155トークンを所有しているアドレスから別のアドレスに送信するための関数です。
+transferbatch(msg.sender, to, ids, values, data);
+//safeTransfersingleは、ERC1155トークンを所有しているアドレスから別のアドレスに送信するための関数です。
+safeTransfersingle(msg.sender, to, id, value, data);
+//safeTransferbatchは、ERC1155トークンを所有しているアドレスから別のアドレスに送信するための関数です。
+safeTransferbatch(msg.sender, to, ids, values, data);
+//setApprovalForAllは、ERC1155トークンを所有しているアドレスが、別のアドレスにトークンを送信することを許可するための関数です。
+setApprovalForAll(operator, approved);
+//isApprovedForAllは、ERC1155トークンを所有しているアドレスが、別のアドレスにトークンを送信することを許可しているかどうかを確認するための関数です。
+isApprovedForAll(owner, operator);
+//balanceofは、ERC1155トークンを所有しているアドレスの残高を確認するための関数です。
+balanceof(owner, id);
+//balanceofbatchは、ERC1155トークンを所有しているアドレスの残高を確認するための関数です。
+balanceofbatch(owners, ids);
+//seturiは、ERC1155トークンのURIを設定するための関数です。
+seturi(_uri);
+//uriは、ERC1155トークンのURIを確認するための関数です。
+uri(id);
+//uriとは、ERC1155トークンのURIを確認するための関数です。
+uri(id);
+//pauseは、ERC1155トークンの送信を一時停止するための関数です。
+pause();
+//unpauseは、ERC1155トークンの送信を再開するための関数です。
+unpause();
+//onERC1155Received(operator, from, id, value, data);
+//onERC1155BatchReceived(operator, from, ids, values, data)とは、ERC1155トークンを受信したときに呼び出される関数です。
+onERC1155Received(operator, from, id, value, data);
+onERC1155BatchReceived(operator, from, ids, values, data);
+//onERC1155Received(operator, from, id, value, data)とは、ERC1155トークンを受信したときに呼び出される関数です。
+onERC1155Received(operator, from, id, value, data);
+onERC1155BatchReceived(operator, from, ids, values, data);
+//onERC1155Received(operator, from, id, value, data)とは、ERC1155トークンを受信したときに呼び出される関数です。
+onERC1155Received(operator, from, id, value, data);
+onERC1155BatchReceived(operator, from, ids, values, data);
+//onERC1155Received(operator, from, id, value, data)とは、ERC1155トークンを受信したときに呼び出される関数です。
+//ERC1155Pausableとは、ERC1155のトークンを所有しているアドレスが、トークンを送信する前に、そのトークンを送信することを許可することができるようにするための機能です。
+bool public isPaused;
+baseURI = "https://api.cryptokitties.co/kitties/";
+//ERC1155Pausableとは、ERC1155のトークンを所有しているアドレスが、トークンを送信する前に、そのトークンを送信することを許可することができるようにするための機能です。
+bool public isPaused;
+baseURI = "https://api.cryptokitties.co/kitties/";
+//safeTransfersingle(msg.sender, to, id, value, data)とは、ERC1155トークンを所有しているアドレスから別のアドレスに送信するための関数です。
+safeTransfersingle(msg.sender, to, id, value, data);
+//safeTransferbatch(msg.sender, to, ids, values, data)とは、ERC1155トークンを所有しているアドレスから別のアドレスに送信するための関数です。
+safeTransferbatch(msg.sender, to, ids, values, data);
+//safeTransfersingle(msg.sender, to, id, value, data)とは、ERC1155トークンを所有しているアドレスから別のアドレスに送信するための関数です。
+safeTransfersingle(msg.sender, to, id, value, data);
+//safeTransferbatch(msg.sender, to, ids, values, data)とは、ERC1155トークンを所有しているアドレスから別のアドレスに送信するための関数です。
+safeTransferbatch(msg.sender, to, ids, values, data);
+
+//isApprovedForAll(owner, operator)とは、ERC1155トークンを所有しているアドレスが、別のアドレスにトークンを送信することを許可しているかどうかを確認するための関数です。
+isApprovedForAll(owner, operator);
+//balanceof(owner, id)とは、ERC1155トークンを所有しているアドレスの残高を確認するための関数です。
+balanceof(owner, id);
+//pause()とは、ERC1155トークンの送信を一時停止するための関数です。
+pause();
+//unpause()とは、ERC1155トークンの送信を再開するための関数です。
+unpause();
+//onERC1155Received(operator, from, id, value, data)とは、ERC1155トークンを受信したときに呼び出される関数です。
+//ERC1155Pausableとは、ERC1155のトークンを所有しているアドレスが、トークンを送信する前に、そのトークンを送信することを許可することができるようにするための機能です。
+bool public isPaused;
+baseURI = "https://api.cryptokitties.co/kitties/";
+//supportsInterface(interfaceId)とは、ERC1155トークンのインターフェースを確認するための関数です。
+supportsInterface(interfaceId);
+//safeTransfersingle(msg.sender, to, id, value, data)とは、ERC1155トークンを所有しているアドレスから別のアドレスに送信するための関数です。
+safeTransfersingle(msg.sender, to, id, value, data);
+//safeTransferbatch(msg.sender, to, ids, values, data)とは、ERC1155トークンを所有しているアドレスから別のアドレスに送信するための関数です。
+safeTransferbatch(msg.sender, to, ids, values, data);
+//safeTransfersingle(msg.sender, to, id, value, data)とは、ERC1155トークンを所有しているアドレスから別のアドレスに送信するための関数です。
+safeTransfersingle(msg.sender, to, id, value, data);
+ownerOf(uint256 tokenId)
+//ownerOf(uint256 tokenId)とは、ERC721トークンを所有しているアドレスを確認するための関数です。
+ownerOf(uint256 tokenId)
+//safeTransferFrom(address from, address to, uint256 tokenId)とは、ERC721トークンを所有しているアドレスから別のアドレスに送信するための関数です。
+safeTransferFrom(address from, address to, uint256 tokenId)
+//supportsInterface(bytes4 interfaceId)とは、ERC721トークンのインターフェースを確認するための関数です。
+supportsInterface(bytes4 interfaceId)
+//safeTransferFrom(address from, address to, uint256 tokenId)とは、ERC721トークンを所有しているアドレスから別のアドレスに送信するための関数です。
+safeTransferFrom(address from, address to, uint256 tokenId)
+//onERC721Received(address operator, address from, uint256 tokenId, bytes data)とは、ERC721トークンを受信したときに呼び出される関数です。
+onERC721Received(address operator, address from, uint256 tokenId, bytes data)
+//isApprovedForAll(address owner, address operator)とは、ERC721トークンを所有しているアドレスが、別のアドレスにトークンを送信することを許可しているかどうかを確認するための関数です。
+isApprovedForAll(address owner, address operator)
+//setApprovalForAll(address operator, bool approved)とは、ERC721トークンを所有しているアドレスが、別のアドレスにトークンを送信することを許可するための関数です。
+setApprovalForAll(address operator, bool approved)
+//getApproved(uint256 tokenId)とは、ERC721トークンを所有しているアドレスが、別のアドレスにトークンを送信することを許可しているかどうかを確認するための関数です。
+getApproved(uint256 tokenId)
+//isApprovedForAll(address owner, address operator)とは、ERC721トークンを所有しているアドレスが、別のアドレスにトークンを送信することを許可しているかどうかを確認するための関数です。
+isApprovedForAll(address owner, address operator)
+//setApprovalForAll(address operator, bool approved)とは、ERC721トークンを所有しているアドレスが、別のアドレスにトークンを送信することを許可するための関数です。
+setApprovalForAll(address operator, bool approved)
+//getApproved(uint256 tokenId)とは、ERC721トークンを所有しているアドレスが、別のアドレスにトークンを送信することを許可しているかどうかを確認するための関数です。
+getApproved(uint256 tokenId)
+//isApprovedForAll(address owner, address operator)とは、ERC721トークンを所有しているアドレスが、別のアドレスにトークンを送信することを許可しているかどうかを確認するための関数です。
+isApprovedForAll(address owner, address operator)
+//setApprovalForAll(address operator, bool approved)とは、ERC721トークンを所有しているアドレスが、別のアドレスにトークンを送信することを許可するための関数です。
+setApprovalForAll(address operator, bool approved)
+//
--- 202210/1021-1031/1024/web3.js
+++ 202210/1021-1031/1024/web3.js
@@ -41,3 +41,124 @@ web3.eth.defaultAccount();
 //web3.eth.getBlockNumber()の関数
 web3.eth.getBlockNumber = function (callback) { }
 //web3.ethとは、web3.ethのインスタンスを生成するという意味
+//web3.eth.getBalance()の定義
+web3.eth.getBalance = function (address, defaultBlock, callback) { }
+//web3.eth.getBalance()の関数
+web3.eth.getBalance = function (address, defaultBlock, callback) { }
+//web3.eth.getStorageAt()のapi
+web3.eth.getStorageAt = function (address, position, defaultBlock, callback) { }
+//web3.eth.getCode()の使い方
+web3.eth.getCode = function (address, defaultBlock, callback) { }
+//web3.eth.getBlock()の使い方
+web3.eth.getBlock = function (blockHashOrBlockNumber, returnTransactionObjects, callback) { }
+//web3.eth.getBlockTransactionCount()を使用すると、ブロックのトランザクション数を取得することができる
+//parameter1:ブロックハッシュまたはブロック番号
+//parameter2:コールバック関数
+web3.eth.getBlockTransactionCount = function (blockHashOrBlockNumber, callback) { }
+//web3.givenProviderとは、web3.givenProviderのインスタンスを生成するという意味
+web3.givenProvider()
+//web3.currentProviderとは、web3.currentProviderのインスタンスを生成するという意味
+web3.currentProvider()
+//new web3.BatchRequest()とは、web3.BatchRequest()のインスタンスを生成するという意味
+new web3.BatchRequest()
+//web3.eth.net.getId()とは、web3.eth.net.getId()のネットワークIDを取得するという意味
+web3.eth.net.getId()
+//web3.eth.net.isListening()とは、web3.eth.net.isListening()のネットワークがリッスンしているかどうかを取得するという意味
+web3.eth.net.isListening()
+//web3.eth.net.getPeerCount()とは、web3.eth.net.getPeerCount()のネットワークのピア数を取得するという意味
+web3.eth.net.getPeerCount()
+//web3.eth.net.getNetworkType()とは、web3.eth.net.getNetworkType()のネットワークの種類を取得するという意味
+web3.eth.net.getNetworkType()
+//web3.eth.net.getNetworkType()の定義
+web3.eth.net.getNetworkType = function (callback) { }
+//web3.eth.Iban()とは、web3.eth.Iban()のインスタンスを生成するという意味
+web3.eth.Iban()
+//ibanとは、Ibanとは、国際銀行口座番号のこと
+web3.eth.Iban.toAddress()
+//web3.eth.Iban.toAddress()とは、web3.eth.Iban.toAddress()のアドレスを取得するという意味
+web3.eth.Iban.toAddress()
+//web3.eth.Iban.fromBban()とは、web3.eth.Iban.fromBban()のインスタンスを生成するという意味
+web3.eth.Iban.fromBban()
+//Bbanとは、Bbanとは、国際銀行口座番号のこと
+web3.eth.Iban.fromBban()
+//bzzとは、bzzとは、swarmのこと
+bzz
+//shhとは、shhとは、whisperのこと
+shh
+//web3.eth.Contract()とは、web3.eth.Contract()のインスタンスを生成するという意味
+web3.eth.Contract()
+//web3.eth.Contract()の定義
+web3.eth.Contract = function (jsonInterface, address, options) { }
+//web3.eth.account.hasMessage()とは、web3.eth.account.hasMessage()のアカウントがメッセージを持っているかどうかを確認するという意味
+web3.eth.account.hasMessage()
+//web3.eth.account.sign()とは、web3.eth.account.sign()のアカウントに署名するという意味
+web3.eth.account.sign()
+//web3.eth.account.recover()とは、web3.eth.account.recover()のアカウントを復元するという意味
+web3.eth.account.recover()
+//web3.eth.account.encrypt()とは、web3.eth.account.encrypt()のアカウントを暗号化するという意味
+web3.eth.account.encrypt()
+//web3.eth.account.decrypt()とは、web3.eth.account.decrypt()のアカウントを復号化するという意味
+web3.eth.account.decrypt()
+//web3.eth.accounts.wallet.create()とは、web3.eth.accounts.wallet.create()のアカウントを作成するという意味
+web3.eth.accounts.wallet.create()
+//web3.eth.accounts.wallet.add()とは、web3.eth.accounts.wallet.add()のアカウントを追加するという意味
+web3.eth.accounts.wallet.add()
+//web3.eth.accounts.wallet.remove()とは、web3.eth.accounts.wallet.remove()のアカウントを削除するという意味
+web3.eth.accounts.wallet.remove()
+//web3.eth.accounts.wallet.clear()とは、web3.eth.accounts.wallet.clear()のアカウントをクリアするという意味
+web3.eth.accounts.wallet.clear()
+//web3.eth.accounts.wallet.encrypt()とは、web3.eth.accounts.wallet.encrypt()のアカウントを暗号化するという意味
+web3.eth.accounts.wallet.encrypt()
+//web3.eth.accounts.wallet.decrypt()とは、web3.eth.accounts.wallet.decrypt()のアカウントを復号化するという意味
+web3.eth.accounts.wallet.decrypt()
+//web3.eth.accounts.wallet.save()とは、web3.eth.accounts.wallet.save()のアカウントを保存するという意味
+web3.eth.accounts.wallet.save()
+//web3.eth.accounts.wallet.load()とは、web3.eth.accounts.wallet.load()のアカウントを読み込むという意味
+web3.eth.accounts.wallet.load()
+//web3.eth.accounts.wallet.encrypt()の定義
+web3.eth.accounts.wallet.encrypt = function (password, options) { }
+//web3.eth.personal.ecRecover()とは、web3.eth.personal.ecRecover()の個人情報を復元するという意味
+web3.eth.personal.ecRecover()
+//web3.eth.personal.sign()とは、web3.eth.personal.sign()の個人情報に署名するという意味
+web3.eth.personal.sign()
+//web3.eth.personal.signTransaction()とは、web3.eth.personal.signTransaction()の個人情報に署名するという意味
+web3.eth.personal.signTransaction()
+//web3.eth.personal.sendTransaction()とは、web3.eth.personal.sendTransaction()の個人情報を送信するという意味
+web3.eth.personal.sendTransaction()
+//web3.eth.personal.unlockAccount()とは、web3.eth.personal.unlockAccount()の個人情報をアンロックするという意味
+web3.eth.personal.unlockAccount()
+//web3.eth.personal.lockAccount()とは、web3.eth.personal.lockAccount()の個人情報をロックするという意味
+web3.eth.personal.lockAccount()
+//web3.eth.personal.importRawKey()とは、web3.eth.personal.importRawKey()の個人情報をインポートするという意味
+web3.eth.personal.importRawKey()
+//web3.eth.personal.newAccount()とは、web3.eth.personal.newAccount()の個人情報を新規作成するという意味
+web3.eth.personal.newAccount()
+//web3.eth.personal.listAccounts()とは、web3.eth.personal.listAccounts()の個人情報をリストアップするという意味
+web3.eth.personal.listAccounts()
+//web3.eth.personal.sign()の定義
+web3.eth.personal.sign = function (dataToSign, address, callback) { }
+//web3.eth.ens.registryAddressとは、web3.eth.ens.registryAddressのENSレジストリアドレスを取得するという意味
+web3.eth.ens.registryAddress
+//web3.eth.ens.registryとは、web3.eth.ens.registryのENSレジストリを取得するという意味
+web3.eth.ens.registry
+//web3.eth.ens.resolverとは、web3.eth.ens.resolverのENSリゾルバを取得するという意味
+web3.eth.ens.resolver
+//web3.eth.ens.resolverとは、web3.eth.ens.resolverのENSリゾルバを取得するという意味
+web3.eth.ens.resolver
+//web3.eth.ens.getResolver()とは、web3.eth.ens.getResolver()のENSリゾルバを取得するという意味
+web3.eth.ens.getResolver()
+//web3.eth.ens.setResolver()とは、web3.eth.ens.setResolver()のENSリゾルバを設定するという意味
+web3.eth.ens.setResolver()
+//web3.eth.ens.getOwner()とは、web3.eth.ens.getOwner()のENSオーナーを取得するという意味
+web3.eth.ens.getOwner()
+//web3.eth.ens.setOwner()とは、web3.eth.ens.setOwner()のENSオーナーを設定するという意味
+web3.eth.ens.setOwner()
+//web3.eth.ens.getContent()とは、web3.eth.ens.getContent()のENSコンテンツを取得するという意味
+web3.eth.ens.getContent()
+//web3.eth.ens.setContent()とは、web3.eth.ens.setContent()のENSコンテンツを設定するという意味
+web3.eth.ens.setContent()
+//web3.eth.ens.setSubnodeOwner()とは、web3.eth.ens.setSubnodeOwner()のENSサブノードオーナーを設定するという意味
+web3.eth.ens.setSubnodeOwner()
+//web3.eth.ens.setSubnodeRecord()とは、web3.eth.ens.setSubnodeRecord()のENSサブノードレコードを設定するという意味
+web3.eth.ens.setSubnodeRecord()
+//web3.eth.ens.registryとは、web3.eth.ens.registryのENSレジストリを取得するという意味
