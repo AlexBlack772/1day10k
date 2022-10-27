6473f9baff9b5bbdd94cf4476f4f0fb46e3ae2be
yoshiaki.n <user44604460@gmail.com>
2022-10-27 13:44:49 +0800

    test

--- 1027/1300/IUniswapV2Pair.sol
+++ 1027/1300/IUniswapV2Pair.sol
@@ -31,6 +31,7 @@ interface IUniswapV2Pair {
       function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
       //price0CumulativeLastとは、最後の価格0の累積
       
+
       
 
 }
