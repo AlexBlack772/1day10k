dd9b48b11ce3176d2fa6427d7b492b7e80284db6
yoshiaki.n <user44604460@gmail.com>
2022-11-10 21:58:38 +0800

    polkDOT

---
+++ 1110/1300/substrate.rs
@@ -0,0 +1,3 @@
+//assert_noopとは、assert_eq!(result, Err(DispatchError::Module { index: 0, error: 0, message: Some("Error message") }));
+assert_noop
+//frame_support::Hashableは、トレイトで、トレイトは、トレイトを実装する型に対して、関連する関数を提供する。
