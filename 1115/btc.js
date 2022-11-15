5342cb424263fb8593d41ea82e81ba46022c5714
yoshiaki.n <user44604460@gmail.com>
2022-11-15 18:15:46 +0800

    final

--- 1115/btc.js
+++ 1115/btc.js
@@ -1,1 +1,74 @@
-//
+const { NodeClient, Network } = require('bcoin');
+const network = Network.get('regtest');
+
+// network type derived from bcoin object, client object stores API key
+const clientOptions = {
+   network: network.type,
+   port: network.rpcPort,
+   apiKey: 'api-key'
+}
+
+const client = new NodeClient(clientOptions);
+
+(async () => {
+   const clientinfo = await client.getInfo();
+   console.log(clientinfo);
+})();
+//Networkとは、bcoinのネットワークの設定を保持するオブジェクト
+//NodeClientとは、bcoinのノードに接続するためのクライアントオブジェクト
+
+const { NodeClient, WalletClient } = require('bclient');
+const { Network } = require('bcoin');
+const network = Network.get('regtest');
+
+const clientOptions = {
+   network: network.type,
+   port: network.rpcPort,
+   apiKey: 'api-key'
+}
+
+const walletOptions = {
+   network: network.type,
+   port: network.walletPort,
+   apiKey: 'api-key'
+}
+
+const client = new NodeClient(clientOptions);
+const wallet = new WalletClient(walletOptions);
+//walletClientはwalletの操作を行うためのクライアントオブジェクト
+//netoworkとは、bcoinのネットワークの設定を保持するオブジェクト
+
+const { NodeClient, Network } = require('bcoin');
+const network = Network.get('regtest');
+
+const clientOptions = {
+   network: network.type,
+   port: network.rpcPort,
+   apiKey: 'api-key'
+}
+
+const client = new NodeClient(clientOptions);
+
+(async () => {
+   const clientinfo = await client.getInfo();
+   console.log(clientinfo);
+})();
+//bcoinのノードに接続するためのクライアントオブジェクト
+const { NodeClient, Network } = require('bcoin');
+const network = Network.get('regtest');
+
+const clientOptions = {
+   network: network.type,
+   port: network.rpcPort,
+   apiKey: 'api-key'
+}
+
+const client = new NodeClient(clientOptions);
+
+(async () => {
+   const mempool = await client.getMempool();
+   console.log(mempool);
+})().catch((err) => {
+   console.error(err.stack);
+});
+//regtestとは、bcoinのネットワークの設定を保持するオブジェクト
---
+++ 1115/ethers.js
@@ -0,0 +1,153 @@
+//singer.connect(provider)とは、singerという変数にproviderという変数を接続するという意味
+singer.connect(provider);
+//singer.getAddress()とは、singerという変数のアドレスを取得するという意味
+singer.getAddress().then((address) => {
+   console.log(address);
+})
+//signer.isSinger()とは、signerという変数がsignerであるかどうかを確認するという意味
+singer.isSigner()
+//singer.getBlance()とは、signerという変数の残高を取得するという意味
+singer.getBalance()
+//get.getGasPrice()とは、getという変数のガス価格を取得するという意味
+get.getGasPrice()
+//singer.getNetwork()とは、singerという変数のネットワークを取得するという意味
+singer.getNetwork()
+//singer.getProvider()とは、singerという変数のプロバイダーを取得するという意味
+singer.getProvider()
+//singer.call()とは、singerという変数の呼び出しを行うという意味
+singer.call()
+//singer.estimateGas()とは、singerという変数のガスの推定を行うという意味
+singer.estimateGas()
+//singer.resolverName()とは、singerという変数の名前の解決を行うという意味
+singer.resolverName()
+//singer.sendTransaction()とは、singerという変数のトランザクションの送信を行うという意味
+singer.sendTransaction()
+//singer.signMessage()とは、singerという変数のメッセージの署名を行うという意味
+singer.signMessage()
+//singer.signTransaction()とは、singerという変数のトランザクションの署名を行うという意味
+singer.signTransaction()
+//singer.sendTransaction()とは、singerという変数のトランザクションの送信を行うという意味
+singer.sendTransaction()
+//singer.checkTransaction()とは、singerという変数のトランザクションの確認を行うという意味
+singer.checkTransaction()
+//singer.getTransactionReceipt()とは、singerという変数のトランザクションのレシートを取得するという意味
+singer.getTransactionReceipt()
+//singer.getTransactionCount()とは、singerという変数のトランザクションのカウントを取得するという意味
+singer.getTransactionCount()
+//new ethers.Wallet()とは、新しいウォレットを作成するという意味
+new ethers.Wallet()
+//new ethers.Wallet.createRandom()とは、新しいランダムなウォレットを作成するという意味
+new ethers.Wallet.createRandom()
+//new ethers.Wallet.fromEncryptedJson()とは、新しい暗号化されたJSONからウォレットを作成するという意味
+new ethers.Wallet.fromEncryptedJson()
+//new ethers.Wallet.fromMnemonic()とは、新しいニーモニックからウォレットを作成するという意味
+new ethers.Wallet.fromMnemonic()
+//new ethers.Wallet.fromEncryptedJsonSync()とは、新しい暗号化されたJSONから同期的にウォレットを作成するという意味
+new ethers.Wallet.fromEncryptedJsonSync()
+//new ethers.Wallet.fromEncryptedJson()とは、新しい暗号化されたJSONからウォレットを作成するという意味
+new ethers.Wallet.fromEncryptedJson()
+//ethers.Wallet.fromMnemonic()とは、ニーモニックからウォレットを作成するという意味
+ethers.Wallet.fromMnemonic()
+//wallet.addressとは、walletという変数のアドレスを取得するという意味
+wallet.address
+//wallet.privateKeyとは、walletという変数のプライベートキーを取得するという意味
+wallet.privateKey
+//wallet.encrypt()とは、walletという変数の暗号化を行うという意味
+wallet.encrypt()
+//ethers.VoidSingerとは、ethersという変数のVoidSingerを取得するという意味
+ethers.VoidSinger
+//voidSinger.addressとは、voidSingerという変数のアドレスを取得するという意味
+voidSinger.address
+//voidSinger.privateKeyとは、voidSingerという変数のプライベートキーを取得するという意味
+voidSinger.privateKey
+//new ethers.Contract()とは、新しいコントラクトを作成するという意味
+new ethers.Contract()
+//contract.addressとは、contractという変数のアドレスを取得するという意味
+contract.address
+//contract.interfaceとは、contractという変数のインターフェースを取得するという意味
+contract.interface
+//contract.providerとは、contractという変数のプロバイダーを取得するという意味
+contract.provider
+//contract.deployed()とは、contractという変数のデプロイを行うという意味
+contract.deployed()
+//contract.deployTransactionとは、contractという変数のデプロイトランザクションを取得するという意味
+contract.deployTransaction
+//contract.functionsとは、contractという変数の関数を取得するという意味
+contract.functions
+//contract.estimateGas()とは、contractという変数のガスの推定を行うという意味
+contract.estimateGas()
+//contract.queryFilter()とは、contractという変数のフィルターのクエリを行うという意味
+contract.queryFilter()
+//contract.listener()とは、contractという変数のリスナーを取得するという意味
+contract.listener()
+//contract.off()とは、contractという変数のオフを行うという意味
+contract.off()
+//contract.on()とは、contractという変数のオンを行うという意味
+contract.on()
+//contract.once()とは、contractという変数のワンを行うという意味
+contract.once()
+//contract.removeAllListeners()とは、contractという変数のリスナーの全削除を行うという意味
+contract.removeAllListeners()
+//contract.removeListener()とは、contractという変数のリスナーの削除を行うという意味
+contract.removeListener()
+//contract.Method()とは、contractという変数のメソッドを取得するという意味
+contract.Method()
+//contract.functions.Method()とは、contractという変数の関数のメソッドを取得するという意味
+contract.functions.Method()
+//contract.estimategas.Method()とは、contractという変数のガスの推定のメソッドを取得するという意味
+contract.estimategas.Method()
+//contract.populateTransaction.Method()とは、contractという変数のトランザクションのメソッドを取得するという意味
+contract.populateTransaction.Method()
+//contract.estimateGas.Method()とは、contractという変数のガスの推定のメソッドを取得するという意味
+contract.estimateGas.Method()
+//contract.queryFilter.Method()とは、contractという変数のフィルターのクエリのメソッドを取得するという意味
+contract.queryFilter.Method()
+//contract.callStatic.Method()とは、contractという変数のスタティックコールのメソッドを取得するという意味
+contract.callStatic.Method()
+//contract.filters.Method()とは、contractという変数のフィルターのメソッドを取得するという意味
+contract.filters.Method()
+//new ethers.ContractFactory()とは、新しいコントラクトファクトリを作成するという意味
+new ethers.ContractFactory()
+//ContractFactory.fromSolidity()とは、コントラクトファクトリをSolidityから作成するという意味
+ContractFactory.fromSolidity()
+//contractFactory.connect()とは、contractFactoryという変数のコネクトを行うという意味
+contractFactory.connect()
+//contractFactory.interfaceとは、contractFactoryという変数のインターフェースを取得するという意味
+contractFactory.interface
+//contractFactory.bytecodeとは、contractFactoryという変数のバイトコードを取得するという意味
+contractFactory.bytecode
+//contractFactory.singerとは、contractFactoryという変数のサイナーを取得するという意味
+contractFactory.singer
+//contaractFactory.attach()とは、contractFactoryという変数のアタッチを行うという意味
+contaractFactory.attach()
+//contractFactory.getDeployTransaction()とは、contractFactoryという変数のデプロイトランザクションを取得するという意味
+contractFactory.deploy()
+//contractFactory.deployed()とは、contractFactoryという変数のデプロイを行うという意味
+contractFactory.deployed()
+//contractFactory.deployTransactionとは、contractFactoryという変数のデプロイトランザクションを取得するという意味
+new ethers.ContractFactory()
+//contractFactory.interfaceとは、contractFactoryという変数のインターフェースを取得するという意味
+contractFactory.interface
+//contractFactory.bytecodeとは、contractFactoryという変数のバイトコードを取得するという意味
+contractFactory.bytecode
+//contractFactory.singerとは、contractFactoryという変数のサイナーを取得するという意味
+contractFactory.singer
+//contaractFactory.attach()とは、contractFactoryという変数のアタッチを行うという意味
+contaractFactory.attach()
+//new ethers.Contract()とは、新しいコントラクトを作成するという意味
+new ethers.Contract()
+//erc20.addressとは、erc20という変数のアドレスを取得するという意味
+erc20.address
+//erc20.interfaceとは、erc20という変数のインターフェースを取得するという意味
+erc20.interface
+//erc20.functionsとは、erc20という変数の関数を取得するという意味
+erc20.functions
+//erc20.estimateGasとは、erc20という変数のガスの推定を取得するという意味
+erc20.estimateGas
+//erc20.populateTransactionとは、erc20という変数のトランザクションを取得するという意味
+erc20.populateTransaction
+//erc20.callStaticとは、erc20という変数のスタティックコールを取得するという意味
+erc20.callStatic
+//erc20.filtersとは、erc20という変数のフィルターを取得するという意味
+erc20.filters
+//
