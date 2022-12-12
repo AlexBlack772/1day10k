5a327a8fc938d64bdb06d5edce9dc9cb6307cdc7
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-12-12 10:24:33 +0800

    1211

---
+++ 2020-/2022/202212/1209-1216/1211/account/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, world!");
+
+}
+
+
+struct Account {
+    name: String,
+    balance: i32,
+}
+
+struct Person {
+    name: String,
+    age: i32,
+    thing: String,
+    money: i32,
+}
+
+
+
