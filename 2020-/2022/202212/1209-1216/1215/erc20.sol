38e4a7e403f44d7543952f94bb3b7e3835c27503
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-12-16 11:37:14 +0800

    1215

---
+++ 2020-/2022/202212/1209-1216/1215/erc20.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.8.0;
+
+contract ERC20 {
+
+   uint public totalSupply;
+   uint public decimal;
+   address public owner;
+   string public name; 
+
+   function withdraw() public {
+
+   }
+
+   function deposit() public {
+
+   }
+
+   function trasfer() public {
+
+   }
+
+   function 
+
+}
