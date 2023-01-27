a36c3a1fba7d4b5576001fcc7ddf3dbfad6e46f3
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2023-01-27 12:06:22 +0800

    example next

--- 0127/daikokuya.sol
+++ 0127/daikokuya.sol
@@ -1,57 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract Car {
-    address public owner;
-    string public model;
-    address public carAddr;
-
-    constructor(address _owner, string memory _model) payable {
-        owner = _owner;
-        model = _model;
-        carAddr = address(this);
-    }
-}
-
-contract CarFactory {
-    Car[] public cars;
-
-    function create(address _owner, string memory _model) public {
-        Car car = new Car(_owner, _model);
-        cars.push(car);
-    }
-
-    function createAndSendEther(address _owner, string memory _model) public payable {
-        Car car = (new Car){value: msg.value}(_owner, _model);
-        cars.push(car);
-    }
-
-    function create2(address _owner, string memory _model, bytes32 _salt) public {
-        Car car = (new Car){salt: _salt}(_owner, _model);
-        cars.push(car);
-    }
-
-    function create2AndSendEther(
-        address _owner,
-        string memory _model,
-        bytes32 _salt
-    ) public payable {
-        Car car = (new Car){value: msg.value, salt: _salt}(_owner, _model);
-        cars.push(car);
-    }
-
-    function getCar(
-        uint _index
-    )
-        public
-        view
-        returns (address owner, string memory model, address carAddr, uint balance)
-    {
-        Car car = cars[_index];
-
-        return (car.owner(), car.model(), car.carAddr(), address(car).balance);
-    }
-}
+//import "/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 interface IShop {
     function foo() external;
@@ -253,15 +203,15 @@ contract Product {
 }
 
 contract ProductFactory {
-    Car[] public cars;
+    Product[] public cars;
 
     function create(address _owner, string memory _model) public {
-        Car car = new Car(_owner, _model);
+        Product car = new Product(_owner, _model);
         cars.push(car);
     }
 
     function createAndSendEther(address _owner, string memory _model) public payable {
-        Car car = (new Car){value: msg.value}(_owner, _model);
+        Product car = (new Product){value: msg.value}(_owner, _model);
         cars.push(car);
     }
 
@@ -272,7 +222,7 @@ contract ProductFactory {
         view
         returns (address owner, string memory model, address carAddr, uint balance)
     {
-        Car car = cars[_index];
+        Product car = cars[_index];
 
         return (car.owner(), car.model(), car.carAddr(), address(car).balance);
     }
